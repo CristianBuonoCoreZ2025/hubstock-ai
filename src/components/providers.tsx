b6dfd2344afa2e6b423568c9816183ae3a8eb70a
Cristian Buono-Core <cristian.buonocore@gmail.com>
2026-05-04 11:24:02 -0400

    modificaciones antes de abacus

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,11 +2,14 @@
 
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from '@/components/ui/sonner'
+import ThemeClientProvider from '@/components/theme/ThemeClientProvider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem storageKey="stockcasa-theme">
-      {children}
+      <ThemeClientProvider>
+        {children}
+      </ThemeClientProvider>
       <Toaster richColors position="top-center" />
     </ThemeProvider>
   )
